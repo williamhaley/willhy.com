8866965b44193d845974e58c4e6cb8d2faf5b243
Will <willhy@gmail.com>
2016-11-05 11:19:38 -0500

    Add an image. Remove moot terraform block.

--- tf/s3.tf
+++ tf/s3.tf
@@ -11,14 +11,6 @@ resource "aws_s3_bucket" "willhy_com" {
 	}
 }
 
-resource "aws_s3_bucket_object" "test_index" {
-	key          = "index.html"
-	bucket       = "${aws_s3_bucket.willhy_com.bucket}"
-	content      = "<html><body>hey</body></html>"
-	acl          = "public-read"
-	content_type = "text/html"
-}
-
 resource "aws_s3_bucket" "www_willhy_com" {
 	bucket = "www.willhy.com"
 	acl    = "public-read"
